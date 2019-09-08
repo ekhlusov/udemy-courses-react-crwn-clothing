69f41956cab80847b46d91d47abadce9db32a363
ekhlusov <ekhlusov@gmail.com>
2019-09-08 22:06:32 +0300

    add form component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
+import SignInSignOut from "./pages/sing-in-sign-out/sing-in-sign-out.component.jsx";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInSignOut} />
       </Switch>
     </div>
   );
---
+++ src/pages/sing-in-sign-out/sing-in-sign-out.component.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./sing-in-sign-out.scss";
+import SignIn from "../../components/sign-in/sign-in.component";
+
+const SignInSignOutPage = () => <div className="sing-in-sign-out">
+    <SignIn />
+</div>;
+
+export default SignInSignOutPage;
