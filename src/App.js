157648241fd08483c2880f47feb3c4ed3050ee27
e.hlusov <e.hlusov@efko.ru>
2019-09-11 11:59:05 +0300

    firebase auth + firebase storage

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInSignOut from "./pages/sing-in-sign-out/sing-in-sign-out.component.jsx";
 
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
 	constructor(props) {
@@ -21,8 +21,21 @@ class App extends React.Component {
 	unsubscribeFromAuth = null;
 
 	componentDidMount() {
-		this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-			this.setState({ currentUser: user });
+		this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+			if (userAuth) {
+				const userRef = await createUserProfileDocument(userAuth);
+
+				userRef.onSnapshot(snapShot => {
+					this.setState({
+						currentUser: {
+							id: snapShot.id,
+							...snapShot.data()
+						}
+					});
+				});
+			}
+
+			this.setState({ currentUser: userAuth });
 		});
 	}
 
