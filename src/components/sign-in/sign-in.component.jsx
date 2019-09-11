d386eb34af7d51304c5215559925d54f7488ce07
e.hlusov <e.hlusov@efko.ru>
2019-09-11 09:03:13 +0300

    buttons + firebase begin

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,48 +1,53 @@
 import React from 'react';
 import './sign-in.scss';
 import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
 
 class SignIn extends React.Component {
-    constructor(props) {
-        super(props);
+	constructor( props ) {
+		super( props );
 
-        this.state = {
-            email: '',
-            password: ''
-        }
-    }
+		this.state = {
+			email: '',
+			password: ''
+		}
+	}
 
-    handleSubmit = e => {
-        e.preventDefault()
+	handleSubmit = e => {
+		e.preventDefault()
 
-        this.setState({email: '', password: ''})
-    }
+		this.setState( { email: '', password: '' } )
+	}
 
-    handleChange = e => {
-        const {value, name} = e.target;
+	handleChange = e => {
+		const { value, name } = e.target;
 
-        this.setState({[name]: value})
-    }
+		this.setState( { [name]: value } )
+	}
 
-    render() {
-        return (
-            <div className='sign-in'>
-                <h2>I already have an account</h2>
-                <span>Sign in with your email and password</span>
+	render() {
+		return (
+			<div className="sign-in">
+				<h2>I already have an account</h2>
+				<span>Sign in with your email and password</span>
 
-                <form onSubmit={this.handleSubmit}>
-                    <FormInput name="email" type='email' value={this.state.email} handleChange={this.handleChange}
-                               label='email'
-                               required/>
-                    <FormInput name="password" type='password' value={this.state.password}
-                               handleChange={this.handleChange}
-                               label="password"
-                               required/>
-                    <input type="submit" value="Submit Form"/>
-                </form>
-            </div>
-        )
-    }
+				<form onSubmit={ this.handleSubmit }>
+					<FormInput
+						name="email" type="email" value={ this.state.email } handleChange={ this.handleChange }
+						label="email"
+						required
+					/>
+					<FormInput
+						name="password" type="password" value={ this.state.password }
+						handleChange={ this.handleChange }
+						label="password"
+						required
+					/>
+					<CustomButton type="submit">Sign in</CustomButton>
+				</form>
+			</div>
+		)
+	}
 
 }
 
