20a8d0eeae73fd3f639abec7ed168fa36ff36673
e.hlusov <e.hlusov@efko.ru>
2019-09-12 11:57:35 +0300

    redux begin

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
+import React from "react";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 
 import { auth, createUserProfileDocument } from "../../firebase/firebase.utils";
 
-import './sign-up.scss'
+import "./sign-up.scss";
 
 class SingUp extends React.Component {
-	constructor( props ) {
-		super( props );
+	constructor(props) {
+		super(props);
 
 		this.state = {
-			displayName: '',
-			email: '',
-			password: '',
-			confirmPassword: '',
-		}
+			displayName: "",
+			email: "",
+			password: "",
+			confirmPassword: ""
+		};
 	}
 
 	handleSubmit = async event => {
@@ -24,30 +24,33 @@ class SingUp extends React.Component {
 		const { displayName, email, password, confirmPassword } = this.state;
 
 		if (password !== confirmPassword) {
-			alert( 'Password don\'t match' );
+			alert("Password don't match");
 			return;
 		}
 
 		try {
-			const { user } = await auth.createUserWithEmailAndPassword( email, password );
+			const { user } = await auth.createUserWithEmailAndPassword(
+				email,
+				password
+			);
 
-			createUserProfileDocument( user, { displayName } );
+			createUserProfileDocument(user, { displayName });
 
-			this.setState( {
-							   displayName: '',
-							   email: '',
-							   password: '',
-							   confirmPassword: ''
-						   } );
+			this.setState({
+				displayName: "",
+				email: "",
+				password: "",
+				confirmPassword: ""
+			});
 		} catch (e) {
-			console.error( e );
+			console.error(e);
 		}
 	};
 
 	handleChange = event => {
 		const { name, value } = event.target;
-		this.setState( { [name]: value } );
-	}
+		this.setState({ [name]: value });
+	};
 
 	render() {
 		const { displayName, email, password, confirmPassword } = this.state;
@@ -56,45 +59,44 @@ class SingUp extends React.Component {
 				<h2 className="title">I do not have a account</h2>
 				<span>Sign up with your email and password</span>
 
-				<form className="sign-up-form" onSubmit={ this.handleSubmit }>
+				<form className="sign-up-form" onSubmit={this.handleSubmit}>
 					<FormInput
 						type="text"
 						name="displayName"
-						value={ displayName }
-						onChange={ this.handleChange }
+						value={displayName}
+						onChange={this.handleChange}
 						label="Display name"
 						required
 					/>
 					<FormInput
 						type="email"
 						name="email"
-						value={ email }
-						onChange={ this.handleChange }
+						value={email}
+						onChange={this.handleChange}
 						label="Email"
 						required
 					/>
 					<FormInput
 						type="password"
 						name="password"
-						value={ password }
-						onChange={ this.handleChange }
+						value={password}
+						onChange={this.handleChange}
 						label="Password"
 						required
 					/>
 					<FormInput
 						type="password"
 						name="confirmPassword"
-						value={ confirmPassword }
-						onChange={ this.handleChange }
+						value={confirmPassword}
+						onChange={this.handleChange}
 						label="Confirm Password"
 						required
 					/>
 					<CustomButton type="submit">SIGN UP</CustomButton>
 				</form>
 			</div>
-		)
+		);
 	}
-
 }
 
 export default SingUp;
--- src/pages/sing-in-sign-out/sing-in-sign-out.component.jsx
+++ src/pages/sing-in-sign-out/sing-in-sign-out.component.jsx
@@ -3,9 +3,11 @@ import "./sing-in-sign-out.scss";
 import SignIn from "../../components/sign-in/sign-in.component";
 import SingUp from "../../components/sign-up/sign-up.component";
 
-const SignInSignOutPage = () => <div className="sing-in-sign-out">
-	<SignIn />
-	<SingUp />
-</div>;
+const SignInSignOutPage = () => (
+	<div className="sing-in-sign-out">
+		<SignIn />
+		<SingUp />
+	</div>
+);
 
 export default SignInSignOutPage;
