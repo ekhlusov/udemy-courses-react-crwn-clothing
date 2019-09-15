d70709844b9123e5d674e650586b851b3d45a050
ekhlusov <ekhlusov@gmail.com>
2019-09-15 21:16:34 +0300

    created selectors + add new reducers

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
-const ShopPage = () => {
+import CollectionPage from "../collection/collection.component";
+const ShopPage = ({ match }) => {
+	// match прилетает из react-router-dom
 	return (
 		<div className="shop-page">
-			<CollectionsOverview />
+			<Route exact path={`${match.path}`} component={CollectionsOverview} />
+			<Route path={`${match.path}/:collectionId`} component={CollectionPage} />
 		</div>
 	);
 };
